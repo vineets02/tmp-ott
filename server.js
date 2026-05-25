1efdc121740a81deb0beab6a4d206090d32ed330
gms-vineet <vineet.shrivastava@getmysolutions.in>
2026-05-25 15:40:45 +0530

    fix: replace invalid react-icons

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.use(
     crossOriginEmbedderPolicy: false,
   })
 ) // Secure HTTP headers
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
 app.use(morgan("dev"))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false, limit: "50mb" }))
 app.use(express.static("public"))
 
 //routes
